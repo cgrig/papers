fa54303b1b8813752a42a4d4cf02b3aaf54fdb59
Radu Grigore <radugrigore@gmail.com>
2011-10-09 09:32:02 +0100

    A few tests for typechecker.

--- agere/tc.ml
+++ agere/tc.ml
@@ -1,13 +1,29 @@
 open Format
 
 type type_ = (string * type_ list) list
-let rec nat : type_= [("zero", []); ("succ", [nat])]
-
 type term = string * term list
-let one : term = "succ", ["zero", []]
+
+let rec nat : type_= ["zero", []; "succ", [nat]; "add", [nat; nat]]
+let rec even : type_ =
+  ["zero", []; "succ", [odd]; "add", [even; even]; "add", [odd; odd]]
+and odd : type_ =
+  ["succ", [even]; "add", [odd; even]; "add", [even; odd]]
+
+let zero : term = "zero", []
+let one : term = "succ", [zero]
+let two : term = "succ", [one]
+let three : term = "succ", [two]
+let three' : term = "add", [one; two]
 
 let rec check v ts =
-  let f (v, vs) (t, ts) = v = t && List.for_all2 check vs ts in
+  let f (v, vs) (t, ts) =
+    v = t &&
+    (try List.for_all2 check vs ts with Invalid_argument _ -> false) in
   List.exists (f v) ts
 
-let () = printf "@[%b@." (check one nat)
+let p v t = printf "@[%b@." (check v t)
+
+let () =
+  p one nat;
+  p one even;
+  p one odd
